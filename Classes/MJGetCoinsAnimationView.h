eefd214116124346b1a7da3adc3ea104b46ef5a5
iosLiuPeng <392009255@qq.com>
2018-08-03 12:28:52 +0800

    修改注释

--- Classes/MJGetCoinsAnimationView.h
+++ Classes/MJGetCoinsAnimationView.h
@@ -6,6 +6,21 @@
 //  Copyright © 2018年 wuhan.musjoy. All rights reserved.
 //
 
+/*
+ 需知：
+ 1.将此视图添加到：动画起点、终点位置共有的父视图上，且与父视图尺寸一致
+ 2.可以设置视图的userInteractionEnabled，来控制动画期间是否可以响应下层视图事件 (默认为此视图不响应事件)
+ 3.支持连续多次开始动画，所以有新的动画时，不需要重新创建视图，直接调用开始方法
+ 4.所有金币动画完成时，会自动将本视图移除
+ 
+ 示例：
+ MJGetCoinsAnimationView *view = [[MJGetCoinsAnimationView alloc] initWithFrame:self.view.bounds];
+ view.imgCoin = [UIImage imageNamed:@"coin"];
+ 
+ [self.view addSubview:view];
+ [view startAnimationFrom:_star.center to:_end.center];
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface MJGetCoinsAnimationView : UIView
@@ -20,13 +35,11 @@
 /**
  开始金币入袋动画
 
- 1.此视图一般作为子视图添加到：动画起点、终点位置视图共有的父视图上
- 2.可以设置视图的userInteractionEnabled，来控制动画期间是否可以响应下层视图事件 (默认为此视图不响应事件)
- 3.支持连续多次开始动画，所以有新的动画时，不需要重新创建视图，直接调用开始方法
- 4.所有金币动画完成时，会自动将本视图移除
- 
  @param fromPoint 金币生成的位置
  @param toPoint 金币最终位置
  */
 - (void)startAnimationFrom:(CGPoint)fromPoint to:(CGPoint)toPoint;
+
+
+
 @end
